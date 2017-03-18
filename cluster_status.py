8f85bc2d94edf6e6297705298f91f67d69cbdf94
J.L. Lanfranchi <jll1062@phys.psu.edu>
2017-03-18 12:15:16 -0400

    fix all the bugs to get first draft working; some hosts I cannot log into, so added to EXCLUDE_HOSTS list

--- cluster_status.py
+++ cluster_status.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
 
+
+# TODO: Kill errant thread(s)
+# TODO: Get memory info for each node to determine if it is memory-limited
+# TODO: Cache results to shared folder with a timestamp so we don't have to
+#       query all hosts to get this info... or separate the "get info" and
+#       "report info" into separate scripts, where one user runs via cron
+#       the "get info" part regularly, and everyone else simply reads the
+#       file produced by that.
+# TODO: Other interesting things...
+
+
 from collections import OrderedDict
 import Queue
 import subprocess
 import threading
+import time
+
+import numpy as np
 
 
 HEADNODE = "aci-b.aci.ics.psu.edu"
-EXCLUDE_HOSTS = ['moab-insight01']
+EXCLUDE_HOSTS = [
+    'moab-insight01',
+    'comp-st-023',
+    'comp-st-027',
+    'comp-st-035',
+    'comp-st-037',
+    'comp-hm-03',
+]
 
 
 def get_pbsnodes_info():
@@ -22,6 +43,8 @@ def get_pbsnodes_info():
     host_sections = out.split('\n\n')
     for host_section in host_sections:
         all_fields = host_section.splitlines()
+        if len(all_fields) == 0:
+            continue
         host_record = OrderedDict()
         host = all_fields[0].strip()
         if host in EXCLUDE_HOSTS:
@@ -52,8 +75,14 @@ def get_load_info(nodes_info):
         threads.append(t)
         t.start()
 
+    # Give some time for commands to complete; when we figure out
+    # how to kill errant threads, we can implement a more subtle
+    # version of this that probably will take less time.
+    time.sleep(10)
+
+    # Do not block...
     for t in threads:
-        t.join()
+        t.join(0.1)
 
     while not q.empty():
         host, load = q.get()
@@ -65,14 +94,23 @@ def get_load_info(nodes_info):
 def get_load(q, host):
     """Get the 15 min load average from `host` and put on queue `q`"""
     out, err = subprocess.Popen(
-        ["ssh", host, "cat /proc/loadavg"],
+        ["ssh", host, "-o StrictHostKeyChecking=no", "cat /proc/loadavg"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     ).communicate()
 
-    load_avg = float(out.split()[3])
+    load_avg = float(out.split()[2])
     q.put((host, load_avg))
 
 
 if __name__ == '__main__':
     nodes_info = get_load_info(get_pbsnodes_info())
+    normalized_loads = []
+    for host, info in nodes_info.iteritems():
+        if 'load' not in info:
+            print host, info
+            continue
+        norm_load = info['load'] / info['np']
+        normalized_loads.append(norm_load)
+        print '%s: np %d, load %3.2f' % (host, info['np'], norm_load)
+    print 'Average load across cluster = %s' % np.mean(normalized_loads)
