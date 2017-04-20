88a789ef6eccaa832461c14af60ef8feee70efa0
J.L. Lanfranchi <jll1062@phys.psu.edu>
2017-04-20 18:08:30 -0400

    replace alias that only lives in justin's bashrc with the `date` command it was calling

--- log_pbsnodes.sh
+++ log_pbsnodes.sh
@@ -4,6 +4,6 @@ mkdir -p /storage/group/dfc13_collab/pbsnodes_logs/
 
 while ((1))
 do
-	pbsnodes | sed -n '/^comp-cl/,/^\s*$/p' > /storage/group/dfc13_collab/pbsnodes_logs/`isodate`
+	pbsnodes | sed -n '/^comp-cl/,/^\s*$/p' > /storage/group/dfc13_collab/pbsnodes_logs/$(date +'%Y-%m-%dT%H%M%z')
 	sleep 3600
 done
