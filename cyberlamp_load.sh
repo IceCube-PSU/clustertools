9e26d0ff8f6227ebbb9a882ab925835fefbb1a99
Philipp Eller <peller@phys.ethz.ch>
2017-05-24 16:11:55 -0400

    make it more lightweight

--- cyberlamp_load.sh
+++ cyberlamp_load.sh
@@ -1,25 +1,28 @@
-gpuusage=`pbsnodes | grep gpu_utilization | tr ';' '\n' | grep gpu_utilization | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
-gpumem=`pbsnodes | grep gpu_memory_used | tr ';' '\n' | grep gpu_memory_used | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
-gpumem_tot=`pbsnodes | grep gpu_memory_total | tr ';' '\n' | grep gpu_memory_total | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
-gputemp=`pbsnodes | grep gpu_utilization | tr ';' '\n' | grep gpu_temp | tr ',' '\n' | grep 'gpu_temp' | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
-ngpus=`pbsnodes | grep gpu_utilization | tr ';' '\n' | grep gpu_utilization -c`
-tot_ngpus=`pbsnodes | grep gpus | tr -s ' ' | cut -d ' ' -f4 | paste -sd+ | bc`
-tot_ncpus=`pbsnodes | grep 'comp-clgc\|comp-clhc' -A 3 | grep np | tr -s ' ' | cut -d ' ' -f4 | paste -sd+ | bc`
+pbsnodesout=`mktemp`
+pbsnodes > $pbsnodesout 2>/dev/null
 
-nodes_down=`pbsnodes | grep 'comp-clgc\|comp-clhc' -A 1 | grep "state = down" -c`
+gpuusage=`cat $pbsnodesout | grep gpu_utilization | tr ';' '\n' | grep gpu_utilization | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
+gpumem=`cat $pbsnodesout | grep gpu_memory_used | tr ';' '\n' | grep gpu_memory_used | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
+gpumem_tot=`cat $pbsnodesout | grep gpu_memory_total | tr ';' '\n' | grep gpu_memory_total | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
+gputemp=`cat $pbsnodesout | grep gpu_utilization | tr ';' '\n' | grep gpu_temp | tr ',' '\n' | grep 'gpu_temp' | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
+ngpus=`cat $pbsnodesout | grep gpu_utilization | tr ';' '\n' | grep gpu_utilization -c`
+tot_ngpus=`cat $pbsnodesout | grep gpus | tr -s ' ' | cut -d ' ' -f4 | paste -sd+ | bc`
+tot_ncpus=`cat $pbsnodesout | grep 'comp-clgc\|comp-clhc' -A 3 | grep np | tr -s ' ' | cut -d ' ' -f4 | paste -sd+ | bc`
 
-cpuusage=`pbsnodes | grep 'comp-clgc\|comp-clhc' | grep status | tr ',' '\n' | grep loadave | grep -Eo '([0-9]*[.])?[0-9]{1,4}' | paste -sd+ | bc`
-ncpus=`pbsnodes | grep 'comp-clgc\|comp-clhc' | grep status | tr ',' '\n' | grep ncpus | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
+nodes_down=`cat $pbsnodesout | grep 'comp-clgc\|comp-clhc' -A 1 | grep "state = down" -c`
 
-gpus_used=`pbsnodes | grep gpu_status | tr ';' '\n' | grep "gpu_state=Exclusive" -c`
-gpus_free=`pbsnodes | grep gpu_status | tr ';' '\n' | grep "gpu_state=Unallocated" -c`
+cpuusage=`cat $pbsnodesout | grep 'comp-clgc\|comp-clhc' | grep status | tr ',' '\n' | grep loadave | grep -Eo '([0-9]*[.])?[0-9]{1,4}' | paste -sd+ | bc`
+ncpus=`cat $pbsnodesout | grep 'comp-clgc\|comp-clhc' | grep status | tr ',' '\n' | grep ncpus | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
+
+gpus_used=`cat $pbsnodesout | grep gpu_status | tr ';' '\n' | grep "gpu_state=Exclusive" -c`
+gpus_free=`cat $pbsnodesout | grep gpu_status | tr ';' '\n' | grep "gpu_state=Unallocated" -c`
 
 cpus_used=`showq -p cyberlamp -r | grep processors | tr -s ' ' | cut -d ' ' -f4`
 cpus_free=$((ncpus - cpus_used))
 
 
-availmem=`pbsnodes | grep 'comp-clgc\|comp-clhc' | grep status | tr ',' '\n' | grep availmem | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
-physmem=`pbsnodes | grep 'comp-clgc\|comp-clhc' | grep status | tr ',' '\n' | grep physmem | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
+availmem=`cat $pbsnodesout | grep 'comp-clgc\|comp-clhc' | grep status | tr ',' '\n' | grep availmem | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
+physmem=`cat $pbsnodesout | grep 'comp-clgc\|comp-clhc' | grep status | tr ',' '\n' | grep physmem | grep -Eo '[0-9]{1,4}' | paste -sd+ | bc`
 av_gpuload=`echo "$gpuusage / $gpus_used" | bc -l`
 av_gpumem=`echo "$gpumem / $gpumem_tot * $ngpus / $gpus_used * 100" | bc -l`
 av_cpuload=`echo "$cpuusage / $cpus_used * 100" | bc -l`
@@ -28,6 +31,8 @@ mem_ratio=`echo "1 - $availmem / $physmem" | bc -l`
 av_mem=`echo " $mem_ratio * 100" | bc -l`
 cpu_ratio=`echo "$cpus_used / $ncpus * 100" | bc -l` 
 gpu_ratio=`echo "$gpus_used / $ngpus * 100" | bc -l` 
+
+rm $pbsnodesout
 
 printf "%s\n" "--- CPU summary ---"
 printf "cluster CPU usage        : %.2f %% (%s cores in use; %s total cores, %s nodes down)\n" $cpu_ratio $cpus_used $cpus_free $nodes_down
