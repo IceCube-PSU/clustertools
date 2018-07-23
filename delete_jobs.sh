a3331d7e85aeca043aaf54fb84089843c0f60514
Philipp Eller <peller@phys.ethz.ch>
2018-07-23 14:16:08 -0400

    bugfix

--- delete_jobs.sh
+++ delete_jobs.sh
@@ -1,2 +1,3 @@
-i=1
-qselect -u $USER $1 | while read x; do qdel $x & if (( i%100==0 )); then wait; fi; i=$(( i+1 )); done
+i=0
+jobs=`qselect -u $USER`
+echo $jobs $1 | while read x; do qdel $x & if (( i%100==0 )); then wait; fi; i=$(( i+1 )); done
